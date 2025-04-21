373616635073b6d298f3dd1587961a11f344369a
ahpicl64 <ubuntu@ip-172-31-2-144.ap-northeast-2.compute.internal>
2025-04-21 19:58:38 +0900

    fix : rbtree.c function location change test/Makefile uncomment -DSENTINEL

--- src/rbtree.c
+++ src/rbtree.c
@@ -20,6 +20,103 @@ void delete_rbtree(rbtree *t) {
     free(t);
 }
 
+void bst_insert(rbtree *t, node_t *p){
+    node_t *cur_node = t->root;
+    node_t *parent_node = t->nil;
+    while (cur_node != t->nil){
+        parent_node = cur_node;
+        if (p->key < cur_node->key){
+            cur_node = cur_node->left;
+        } else {
+            cur_node = cur_node->right;
+        }
+    }
+    p->parent = parent_node;
+    if (parent_node == t->nil){
+        t->root = p;
+    } else if (p->key < parent_node->key){
+        parent_node->left = p;
+    } else {
+        parent_node->right = p;
+    }
+    return;
+}
+
+void left_rotate(rbtree *t, node_t *parent_node){
+    node_t *child_node = parent_node->right;
+    parent_node->right = child_node->left;
+    if(child_node != t->nil){
+        child_node->left->parent = parent_node;
+    }
+
+    // If the original parent was the tree root, set root to the child
+    // otherwise update the childe_node pointer to grandparent's left or right 
+    if(parent_node->parent == t->nil){
+        t->root = child_node;
+    } else if (parent_node == parent_node->parent->left){
+        parent_node->parent->left = child_node;
+    } else {
+        parent_node->parent->right = child_node;
+    }
+    child_node->left = parent_node;
+    parent_node->parent = child_node;
+}
+
+void right_rotate(rbtree *t, node_t *parent_node){
+    node_t *child_node = parent_node->left;
+    parent_node->left = child_node->right;
+    if(child_node != t->nil){
+        child_node->right->parent = parent_node;
+    }
+  
+    // If the original parent was the tree root, set root to the child
+    // otherwise update the childe_node pointer to grandparent's left or right 
+    if(parent_node->parent == t->nil){
+        t->root = child_node;
+    } else if (parent_node == parent_node->parent->left){
+        parent_node->parent->left = child_node;
+    } else {parent_node->parent->right = child_node;}
+    child_node->left = parent_node;
+    parent_node->parent = child_node;
+}
+
+void rb_insert_fix_up(rbtree *t, node_t *p){
+    while (p->parent->color == RBTREE_RED){
+        if (p->parent == p->parent->parent->left){      // parent's location is grandparent's left side
+            // uncle's color is RBTREE_RED (Case 1)
+            if (p->parent->parent->right->color == RBTREE_RED){   // recoloring
+                p->parent->color = RBTREE_BLACK;
+                p->parent->parent->right->color = RBTREE_BLACK;
+                p->parent->parent->color = RBTREE_RED;
+                rb_insert_fix_up(t, p->parent->parent);
+            } else {  // uncle's color is RBTREE_BLACK
+                if (p == p->parent->right){                 // Tree is L-R pattren (inner case) (Case 2)
+                    p = p->parent;
+                    left_rotate(t, p->parent);
+                    }                                           // Tree is L-L pattren (outer case) (Case 3)
+                p->parent->color = RBTREE_BLACK;
+                p->parent->parent->color = RBTREE_RED;
+                right_rotate(t, p->parent->parent);
+            }
+        } else {                        // parent->right == p
+            if (p->parent->parent->left->color == RBTREE_RED){   // recoloring
+                p->parent->color = RBTREE_BLACK;
+                p->parent->parent->left->color = RBTREE_BLACK;
+                p->parent->parent->color = RBTREE_RED;
+                rb_insert_fix_up(t, p->parent->parent);
+            } else {  // uncle's color is RBTREE_BLACK
+                if (p == p->parent->left){                 // Tree is L-R pattren (inner case) (Case 2)
+                    p = p->parent;
+                    right_rotate(t, p->parent);
+                }                                           // Tree is L-L pattren (outer case) (Case 3)
+                p->parent->color = RBTREE_BLACK;
+                p->parent->parent->color = RBTREE_RED;
+                left_rotate(t, p->parent->parent);
+            }
+        }
+    }
+}
+
 node_t *rbtree_insert(rbtree *t, const key_t key) {
     // TODO: implement insert
     // 삽입 노드 초기화
@@ -62,99 +159,4 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
     return 0;
 }
 
-void bst_insert(rbtree *t, node_t *p){
-    node_t *cur_node = t->root;
-    node_t *parent_node = t->nil;
-    while (cur_node != t->nil){
-        parent_node = cur_node;
-        if (p->key < cur_node->key){
-            cur_node = cur_node->left;
-        } else {
-            cur_node = cur_node->right;
-        }
-    }
-    p->parent = parent_node;
-    if (parent_node == t->nil){
-        t->root = p;
-    } else if (p->key < parent_node->key){
-        parent_node->left = p;
-    } else {
-        parent_node->right = p;
-    }
-    return;
-}
 
-void rb_insert_fix_up(rbtree *t, node_t *p){
-    while (p->parent->color == RBTREE_RED){
-        if (p->parent == p->parent->parent->left){      // parent's location is grandparent's left side
-            // uncle's color is RBTREE_RED (Case 1)
-            if (p->parent->parent->right->color == RBTREE_RED){   // recoloring
-                p->parent->color = RBTREE_BLACK;
-                p->parent->parent->right->color = RBTREE_BLACK;
-                p->parent->parent->color = RBTREE_RED;
-                rb_insert_fix_up(t, p->parent->parent);
-            } else {  // uncle's color is RBTREE_BLACK
-                if (p == p->parent->right){                 // Tree is L-R pattren (inner case) (Case 2)
-                    p = p->parent;
-                    left_rotate(t, p->parent);
-                    }                                           // Tree is L-L pattren (outer case) (Case 3)
-                p->parent->color = RBTREE_BLACK;
-                p->parent->parent->color = RBTREE_RED;
-                right_rotate(t, p->parent->parent);
-            }
-        } else {                        // parent->right == p
-            if (p->parent->parent->left->color == RBTREE_RED){   // recoloring
-                p->parent->color = RBTREE_BLACK;
-                p->parent->parent->left->color = RBTREE_BLACK;
-                p->parent->parent->color = RBTREE_RED;
-                rb_insert_fix_up(t, p->parent->parent);
-            } else {  // uncle's color is RBTREE_BLACK
-                if (p == p->parent->left){                 // Tree is L-R pattren (inner case) (Case 2)
-                    p = p->parent;
-                    right_rotate(t, p->parent);
-                }                                           // Tree is L-L pattren (outer case) (Case 3)
-                p->parent->color = RBTREE_BLACK;
-                p->parent->parent->color = RBTREE_RED;
-                left_rotate(t, p->parent->parent);
-            }
-        }
-    }
-}
-
-void left_rotate(rbtree *t, node_t *parent_node){
-    node_t *child_node = parent_node->right;
-    parent_node->right = child_node->left;
-    if(child_node != t->nil){
-        child_node->left->parent = parent_node;
-    }
-
-    // If the original parent was the tree root, set root to the child
-    // otherwise update the childe_node pointer to grandparent's left or right 
-    if(parent_node->parent == t->nil){
-        t->root = child_node;
-    } else if (parent_node == parent_node->parent->left){
-        parent_node->parent->left = child_node;
-    } else {
-        parent_node->parent->right = child_node;
-    }
-    child_node->left = parent_node;
-    parent_node->parent = child_node;
-}
-
-void right_rotate(rbtree *t, node_t *parent_node){
-    node_t *child_node = parent_node->left;
-    parent_node->left = child_node->right;
-    if(child_node != t->nil){
-        child_node->right->parent = parent_node;
-    }
-  
-    // If the original parent was the tree root, set root to the child
-    // otherwise update the childe_node pointer to grandparent's left or right 
-    if(parent_node->parent == t->nil){
-        t->root = child_node;
-    } else if (parent_node == parent_node->parent->left){
-        parent_node->parent->left = child_node;
-    } else {parent_node->parent->right = child_node;}
-    child_node->left = parent_node;
-    parent_node->parent = child_node;
-}
