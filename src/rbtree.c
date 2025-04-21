e8723027b0b6c44ff21259825279bfcfd18f5f8d
ahpicl64 <ubuntu@ip-172-31-2-144.ap-northeast-2.compute.internal>
2025-04-21 11:46:21 +0900

    feat(rbtree.c) : 트리 초기화, BST 방식 삽입함수 구현

--- src/rbtree.c
+++ src/rbtree.c
@@ -5,6 +5,13 @@
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
+  p->nil = malloc(sizeof (node_t));
+  p->nil->color = RBTREE_BLACK;
+  p->nil->left = p->nil;
+  p->nil->right = p->nil;
+  p->nil->parent_node = p->nil;
+
+  p->root = p->nil;
   return p;
 }
 
@@ -15,6 +22,17 @@ void delete_rbtree(rbtree *t) {
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
+  // 삽입 노드 초기화
+  node = malloc(sizeof(node_t));
+  node->key = key
+  node->left = t->nil;
+  node->right = t->nil;
+  node->parent = t->nil;
+  node->color = red;
+
+  bst_insert(t, node);\
+
+
   return t->root;
 }
 
@@ -41,4 +59,30 @@ int rbtree_erase(rbtree *t, node_t *p) {
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   // TODO: implement to_array
   return 0;
+}
+
+void bst_insert(rbtree *t, node_t *p){
+  node_t *cur_node = t->root;
+  node_t *parent_node = t->nil;
+  while (cur_node != nil){
+    parent_node = cur_node
+    if (p->key < cur_node->key){
+      cur_node = cur_node->left;
+    } else {
+      cur_node = cur_node->right;
+    }
+  }
+  p->parent = parent_node;
+  if (parent_node == t->nil){
+    t->root = p;
+  } else if (p->key < parent_node->key){
+    parent_node->left = p;
+  } else {
+    parent_node->right = p;
+  }
+  return;
+}
+
+void rb_insert_fix_up(rbtree *t, node_t *p){
+
 }
